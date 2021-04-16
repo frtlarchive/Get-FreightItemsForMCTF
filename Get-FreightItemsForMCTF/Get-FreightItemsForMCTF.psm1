c2161826811be706462e07b5b088fc971a775d5a
Roy Ashbrook <rashbrook@patriottrans.com>
2021-04-16 17:42:48 -0400

    v1.0.0.1

--- Get-FreightItemsForMCTF/Get-FreightItemsForMCTF.psm1
+++ Get-FreightItemsForMCTF/Get-FreightItemsForMCTF.psm1
@@ -1,13 +1,16 @@
-Import-Module SqlServer
 #This function returns a list of Freight Items to be used for Motor Carrier Tax Filing
-#Takes a file with sql and a connection string, retrieves data using invoke-sqlcmd
-# saves data to an output file and reimports it to normalize the input with other
-# processes that start with csv and finally returns the data.
-function Get-FreightItemsForMCTF($sql,$cs,$of_FreightItemsAll){
-    $FreightItems = Invoke-Sqlcmd -InputFile $sql -ConnectionString $cs -QueryTimeout 1800 |
-        Select-Object * -ExcludeProperty RowError,RowState,Table,ItemArray,HasErrors
-    $FreightItems | Export-Csv -NoTypeInformation $of_FreightItemsAll
-    $FreightItems = Import-Csv $of_FreightItemsAll
-    $FreightItems
+function Get-FreightItemsForMCTF($sqlfile,$cs,$of_FreightItemsAll){
+    if(Test-Path $sqlfile){
+        $sql = Get-Content $sqlfile -Raw
+        $dt = new-object System.Data.DataTable
+        $da = new-object System.Data.SqlClient.SqlDataAdapter $sql,$cs 
+        $da.SelectCommand.CommandTimeout = 1800
+        $null = $da.Fill($dt)
+        $null = $da.Dispose()
+        $dt |
+            Select-Object * -ExcludeProperty RowError,RowState,Table,ItemArray,HasErrors |
+            Export-Csv -NoTypeInformation $of_FreightItemsAll
+        Import-Csv $of_FreightItemsAll
+    }
 }
-Export-ModuleMember -Function * -Alias *
+Export-ModuleMember -Function 'Get-FreightItemsForMCTF'
